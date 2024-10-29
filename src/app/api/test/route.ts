77d55146e4e683450b2140f160e45da66646e159
xspooky7 <c.eissler123@gmail.com>
2024-10-30 00:44:02 +0100

    setting api auth

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,10 @@
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(req: Request) {
+	const headersList = await headers()
+	const apiKey = headersList.get('x-api-key')
+	if (apiKey !== process.env.UPDATE_API_KEY)
+		return new NextResponse('Unauthorized', { status: 401 })
 	return NextResponse.json({ ja: 'nein' })
 }
