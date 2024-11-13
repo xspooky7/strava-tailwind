4daa8b5ce16316e83b6cdad5f42b93aee0dccb8e
xspooky7 <c.eissler123@gmail.com>
2024-11-13 07:24:38 +0100

    feat: error handler

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
+}
+
+export const asError = (thrown: unknown): Error => {
+  if (thrown instanceof Error) return thrown
+  try {
+    return new Error(JSON.stringify(thrown))
+  } catch {
+    return new Error(String(thrown))
+  }
 }
