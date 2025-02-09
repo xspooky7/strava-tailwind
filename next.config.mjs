44616161e00e55a078cf0bb8bdda86af46e5d888
xspooky7 <c.eissler123@gmail.com>
2025-02-09 05:52:27 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["http://195.201.112.203/"],
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
